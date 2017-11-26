1febdee6f8f8d41ae3d6ad7515c3d018b8d2776c
Joemaru <joe.maru.60195@gmail.com>
2017-11-26 13:42:36 +0900

    初期

---
+++ WeatherApplication_VB/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(210, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "神奈川県東部（横浜）の天気"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(10, 100)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 12)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 120)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 12)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Label6"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ WeatherApplication_VB/Form1.vb
@@ -0,0 +1,90 @@
+﻿
+Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Text
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'ダウンロード元のURL
+        Dim urlName As String = "https://weather.yahoo.co.jp/weather/jp/14/4610.html"
+        'WebClientを作成
+        Dim wc As New System.Net.WebClient()
+        '文字コードを指定
+        wc.Encoding = System.Text.Encoding.UTF8
+        'データを文字列としてダウンロードする
+        Dim source As String = wc.DownloadString(urlName)
+        '後始末
+        wc.Dispose()
+        'ダウンロードしたデータを表示する
+        Console.WriteLine(source)
+
+        Dim wc2 As WebClient = New WebClient()
+        Dim st As Stream = wc2.OpenRead("https://weather.yahoo.co.jp/weather/jp/14/4610.html")
+        Dim enc As Encoding = Encoding.GetEncoding("utf-8")
+        Dim sr As StreamReader = New StreamReader(st, enc)
+        Dim html As String = sr.ReadToEnd()
+
+        sr.Close()
+        st.Close()
+
+        'Console.WriteLine(html)
+
+        'Dim weatherdate As String       '日付
+        'Dim weather As String           '天気
+        'Dim hightemp As String          '最高気温
+        'Dim lowtemp As String           '最低気温
+        'Dim precipitation As String     '降水確率
+
+        Dim announce As Integer = InStr(source, "yjSt yjw_note_h2")  '発表日時
+        Dim adate1 As Integer = announce + 18
+        Dim adate2 As Integer = InStr(adate1, source, "</p>")
+        Dim length1 As Integer = adate2 - adate1
+        Dim happyo As String = source.Substring(adate1, length1 - 1)
+        Label2.Text = happyo
+
+        Dim weatherdate As Integer = InStr(adate2, source, "<p class")
+        Dim wdate1 As Integer = weatherdate + 15
+        Dim wdate2 As Integer = InStr(wdate1, source, "</p>")
+        Dim length2 As Integer = wdate2 - wdate1
+        Dim wdate As String = source.Substring(wdate1, length2 - 1)
+        Label3.Text = wdate
+
+        Dim weather As Integer = InStr(wdate2, source, "alt=")
+        Dim weather1 As Integer = InStr(weather, source, ">")
+        Dim weather2 As Integer = InStr(weather1, source, "</p>")
+        Dim length3 As Integer = weather2 - weather1
+        Dim wforcast As String = source.Substring(weather1, length3 - 1)
+        Label4.Text = wforcast
+
+        Dim hightemp As Integer = InStr(weather2, source, "<em>")
+        Dim hightemp1 As Integer = hightemp + 3
+        Dim hightemp2 As Integer = InStr(hightemp1, source, "</li>")
+        Dim length4 As Integer = hightemp2 - hightemp1
+        Dim hforcast As String = source.Substring(hightemp1, length4 - 1)
+        Label5.Text = "最高気温：　" + hforcast.Replace("</em>", "")
+
+        Dim lowtemp As Integer = InStr(hightemp2, source, "<em>")
+        Dim lowtemp1 As Integer = lowtemp + 3
+        Dim lowtemp2 As Integer = InStr(lowtemp1, source, "</li>")
+        Dim length5 As Integer = lowtemp2 - lowtemp1
+        Dim lforcast As String = source.Substring(lowtemp1, length5 - 1)
+        Label6.Text = "最低気温：　" + lforcast.Replace("</em>", "")
+
+
+        'lowtemp = InStr(announce, "low", source)
+        'precipitation = InStr(announce, "precip", source)
+
+        Console.WriteLine(lowtemp1)
+        Console.WriteLine(lowtemp2)
+        Console.WriteLine(length5)
+        Console.WriteLine(lforcast)
+
+    End Sub
+
+
+
+End Class
